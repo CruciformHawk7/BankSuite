7f00261b8e5146bdb21b111fb4b344f25d03f329
Nikhil Hari <nikhil_hari@live.com>
2019-09-10 23:42:20 +0530

    Connect GridPane to Stage to demo the program

--- src/main/java/nikhil/banksuite/App.java
+++ src/main/java/nikhil/banksuite/App.java
@@ -4,11 +4,25 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * A simple GUI Java application that demonstrates a sample bank 
+ * With accounts that can be withdrawn from and transferred to
+ * run: mvn clean javafx:run
+ *
+ */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        
+        ClientUI clui = new ClientUI(DemoDataGenerator.generateFakeClient());
+        try {
+        DemoDataGenerator.writeToFile("test.txt");
+        } catch(Exception e) {
+            System.exit(0);
+        }
+        Scene scene = new Scene(clui.homeScreen, 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
