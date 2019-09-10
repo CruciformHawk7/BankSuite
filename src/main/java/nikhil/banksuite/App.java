79ec98247c0b1b42c0806a200a6b64433fc59e1b
Nikhil Hari <nikhil_hari@live.com>
2019-09-11 01:42:40 +0530

    Change dimensions and add Title to stage

--- src/main/java/nikhil/banksuite/App.java
+++ src/main/java/nikhil/banksuite/App.java
@@ -20,8 +20,9 @@ public class App extends Application {
         } catch(Exception e) {
             System.exit(0);
         }
-        Scene scene = new Scene(clui.homeScreen, 640, 480);
+        Scene scene = new Scene(clui.homeScreen, 1024, 576);
         stage.setScene(scene);
+        stage.setTitle(clui.getName());
         stage.show();
     }
 
