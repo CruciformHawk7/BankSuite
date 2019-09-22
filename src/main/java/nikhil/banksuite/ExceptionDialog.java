5e2042bf25d4e04d243a31d48e53835da9ac360b
Nikhil Hari <nikhil_hari@live.com>
2019-09-22 16:44:23 +0530

    Add Custom Exception Dialog

---
+++ src/main/java/nikhil/banksuite/ExceptionDialog.java
@@ -0,0 +1,69 @@
+package nikhil.banksuite;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+
+class ExceptionDialog {
+
+    String title;
+    String headerText;
+    String contentText;
+    Exception e;
+
+    ExceptionDialog(Exception e) {
+        this.title = e.getClass().getName();
+        this.headerText = e.getCause().toString();
+        this.contentText = e.getMessage();
+        this.e = e;
+    }
+
+    ExceptionDialog(String title, Exception e) {
+        this.title = title;
+        this.headerText = e.getCause().toString();
+        this.contentText = e.getMessage();
+        this.e = e;
+    }
+
+    ExceptionDialog(String title, String headerText, Exception e) {
+        this.title = title;
+        this.headerText = headerText;
+        this.contentText = e.getMessage();
+        this.e = e;
+    }
+
+    public void show() {
+        Alert box = new Alert(AlertType.ERROR);
+        box.setTitle(title);
+        box.setHeaderText(headerText);
+        box.setContentText(contentText);
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        String fullTrace = sw.toString();
+
+        TextArea stackTrace = new TextArea(fullTrace);
+        GridPane gp = new GridPane();
+        RowConstraints r1 = new RowConstraints();
+        r1.setMaxHeight(14);
+        RowConstraints r2 = new RowConstraints();
+
+        ColumnConstraints column = new ColumnConstraints();
+
+        gp.getRowConstraints().addAll(r1,r2);
+        gp.getColumnConstraints().add(column);
+
+        gp.add(new Label("Stacktrace:"), 0, 0);
+        gp.add(stackTrace, 0, 1);
+
+        box.getDialogPane().setExpandableContent(gp);
+        box.showAndWait();
+    }
+}
